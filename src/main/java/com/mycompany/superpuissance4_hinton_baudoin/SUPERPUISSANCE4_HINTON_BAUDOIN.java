8536a75ef6cd2db9cfa17fe3b8ada8d3193ed495
thomas hinton <thomas.hinton@epfedu.fr>
2022-11-18 15:10:58 +0100

    Teste avec Mr Baudoin : -supression ligne hello world

--- src/main/java/com/mycompany/superpuissance4_hinton_baudoin/SUPERPUISSANCE4_HINTON_BAUDOIN.java
+++ src/main/java/com/mycompany/superpuissance4_hinton_baudoin/SUPERPUISSANCE4_HINTON_BAUDOIN.java
@@ -12,6 +12,5 @@ package com.mycompany.superpuissance4_hinton_baudoin;
 public class SUPERPUISSANCE4_HINTON_BAUDOIN {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
