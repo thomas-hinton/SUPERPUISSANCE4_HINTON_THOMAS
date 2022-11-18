15aa3d876632e9f77fac03dd9baadfd4f3e02f3c
thomas hinton <thomas.hinton@epfedu.fr>
2022-11-18 14:56:35 +0100

    Création du projet Super puissance 4: -Création d'un dépot conjoint avec Mr Baudoin et M Hinton

---
+++ src/main/java/com/mycompany/superpuissance4_hinton_baudoin/SUPERPUISSANCE4_HINTON_BAUDOIN.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.superpuissance4_hinton_baudoin;
+
+/**
+ *
+ * @author 33621
+ */
+public class SUPERPUISSANCE4_HINTON_BAUDOIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
